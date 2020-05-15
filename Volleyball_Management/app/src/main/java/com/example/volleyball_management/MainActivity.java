c175b00fc8d66c4b5f67160d4483af5a3d6596c4
liluodii <liluodedra13@gmail.com>
2020-05-15 16:04:54 -0400

    Folders for All Files and Project with Splash Screen Added

---
+++ Volleyball_Management/app/src/main/java/com/example/volleyball_management/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.volleyball_management;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPASH_SCREEN=5000;
+
+    Animation topanim,bottomanim;
+    ImageView image;
+    TextView logo,slogan;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        //Animation
+
+        topanim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomanim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        image=findViewById(R.id.imageViewlogo);
+        logo=findViewById(R.id.tvlogo);
+        slogan=findViewById(R.id.tvslogan);
+
+        image.setAnimation(topanim);
+        logo.setAnimation(bottomanim);
+        slogan.setAnimation(bottomanim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(MainActivity.this,Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPASH_SCREEN);
+    }
+}
