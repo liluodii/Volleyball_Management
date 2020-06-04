5db4bfc057f62d223b8c6e70946d08d2f65ca348
pulkit175 <pulkit175>
2020-06-05 00:18:36 +0530

    -- Add / Edit Team manager -- Delete Team manager

--- API_Data/Volleyball.EF/CustomClasses/CRequest.cs
+++ API_Data/Volleyball.EF/CustomClasses/CRequest.cs
@@ -52,7 +52,28 @@ namespace Volleyball.EF.CustomClasses
             public string Address { get; set; }
         }
 
-       
+
+        public class CReqAddEditTeamManager
+        {
+            public string APIKey { get; set; }
+            public int UserID { get; set; } = 0;
+
+            public string EmailID { get; set; }
+            public string FirstName { get; set; }
+
+            public string Password { get; set; }
+            public string LastName { get; set; }
+
+            public string Gender { get; set; }
+            public string Contact { get; set; }
+            public string DOB { get; set; }
+
+            public string JoinDate { get; set; }
+            
+            public string Address { get; set; }
+        }
+
+
 
 
         public class CReqForgetPassword
