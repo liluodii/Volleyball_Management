bed9e70b3488893601f5f6dbfb0329935e845720
urviarora8115 <urviarora8115@gmail.com>
2020-06-18 18:30:52 +0530

    add missing files

---
+++ API_Data/VolleyballAPI/Controllers/TournamentController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Web.Http;
+
+using Volleyball.Common;
+
+using VolleyballService.Services;
+using static Volleyball.EF.CustomClasses.CRequest;
+
+
+
+namespace VolleyballAPI.Controllers
+{
+    public class TournamentController : ApiController
+    {
+        public BaseService _BaseService = new BaseService();
+        TournamentService _Service = new TournamentService();
+
+
+        [Route("AddEditTournamet")]
+        [HttpPut]
+        public GenericClass AddEditTournamet(CReqAddEditTournament Data)
+        {
+            GenericClass Obj = new GenericClass();
+            try
+            {
+                if (Data == null)
+                {
+
+                    Obj.ReturnCode = ResponseMessages.NoDataCode;
+                    Obj.ReturnMsg = "Please enter valid data";
+                    Obj.ReturnValue = string.Empty;
+                    return Obj;
+                }
+
+                bool _IsValidToken = _BaseService.ValidateAPIToken(Data.APIKey);
+                if (_IsValidToken == true)
+                {
+                    Obj = _Service.AddEditTournamet(Data);
+                }
+                else
+                {
+                    Obj.ReturnCode = ResponseMessages.AuthenticationFailedCode;
+                    Obj.ReturnMsg = ResponseMessages.AuthenticationFailedMsg;
+                    Obj.ReturnValue = string.Empty;
+
+                }
+            }
+#pragma warning disable CS0168 // The variable 'ex' is declared but never used
+            catch
+#pragma warning restore CS0168 // The variable 'ex' is declared but never used
+            {
+                Obj.ReturnCode = ResponseMessages.ErrorCode;
+                Obj.ReturnMsg = ResponseMessages.ErrorMsg;
+                Obj.ReturnValue = string.Empty;
+            }
+            return Obj;
+        }
+
+
+        [Route("DeleteTournament")]
+        [HttpDelete]
+        public GenericClass DeleteTournament(CReqDeleteTournament Data)
+        {
+            GenericClass Obj = new GenericClass();
+            try
+            {
+                if (Data == null)
+                {
+
+                    Obj.ReturnCode = ResponseMessages.NoDataCode;
+                    Obj.ReturnMsg = "Please enter valid data";
+                    Obj.ReturnValue = string.Empty;
+                    return Obj;
+                }
+
+                bool _IsValidToken = _BaseService.ValidateAPIToken(Data.APIKey);
+                if (_IsValidToken == true)
+                {
+                    Obj = _Service.DeleteTournament(Data);
+                }
+                else
+                {
+                    Obj.ReturnCode = ResponseMessages.AuthenticationFailedCode;
+                    Obj.ReturnMsg = ResponseMessages.AuthenticationFailedMsg;
+                    Obj.ReturnValue = string.Empty;
+
+                }
+            }
+#pragma warning disable CS0168 // The variable 'ex' is declared but never used
+            catch
+#pragma warning restore CS0168 // The variable 'ex' is declared but never used
+            {
+                Obj.ReturnCode = ResponseMessages.ErrorCode;
+                Obj.ReturnMsg = ResponseMessages.ErrorMsg;
+                Obj.ReturnValue = string.Empty;
+            }
+            return Obj;
+        }
+
+
+    }
+}
---
+++ API_Data/VolleyballService/Services/TournamentService.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Collections.Generic;
+using static Volleyball.EF.CustomClasses.CRequest;
+using static Volleyball.EF.CustomClasses.CResponse;
+using Volleyball.EF;
+using Volleyball.Common;
+using System.Net;
+using System.IO;
+using System.Net.Mail;
+using System.Net.Mime;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace VolleyballService.Services
+{
+    public class TournamentService : CConnect
+    {
+
+        public GenericClass AddEditTournamet(CReqAddEditTournament Data)
+        {
+            GenericClass obj = new GenericClass();
+
+            try
+            {
+
+
+                if (Data.TournamentID != 0)
+                {
+                    Tournament TM = (from u in DC.Tournaments
+                                     where u.ID == Data.TournamentID
+                                     select u).FirstOrDefault();
+
+
+
+                    if (TM != null)
+                    {
+                        TM.EndDate = Data.EndDate;
+                        TM.ModifiedDate = System.DateTime.UtcNow;
+                        TM.Name = Data.Name;
+                        TM.StartDate = Data.StartDate;
+                        DC.SaveChanges();
+                        obj.ReturnMsg = "Successfully update tournament.";
+
+                    }
+                    else
+                    {
+                        obj.ReturnCode = ResponseMessages.NoDataCode;
+                        obj.ReturnMsg = "Tournament does not exist";
+
+                        return obj;
+                    }
+
+                }
+
+                else
+                {
+
+
+                    Tournament TM = new Tournament();
+
+                    TM.StartDate = Data.StartDate;
+                    TM.EndDate = Data.EndDate;
+                    TM.Name = Data.Name;
+                    TM.CreatedDate = System.DateTime.UtcNow;
+                    DC.Tournaments.Add(TM);
+                    DC.SaveChanges();
+                    obj.ReturnMsg = "Successfully add Tournament.";
+                }
+                obj.ReturnCode = ResponseMessages.SuccessCode;
+
+            }
+            catch (Exception EX)
+            {
+
+                throw;
+            }
+
+            return obj;
+        }
+
+        public GenericClass DeleteTournament(CReqDeleteTournament Data)
+        {
+            GenericClass obj = new GenericClass();
+
+            try
+            {
+                Tournament tournament = (from u in DC.Tournaments
+                                         where u.ID == Data.TournamentID
+                                         select u).FirstOrDefault();
+                if (tournament != null)
+                {
+
+                    DC.Tournaments.Remove(tournament);
+                    DC.SaveChanges();
+
+                }
+                else
+                {
+                    obj.ReturnCode = ResponseMessages.NoDataCode;
+                    obj.ReturnMsg = "Tournament does not exist";
+                    return obj;
+                }
+                obj.ReturnCode = ResponseMessages.SuccessCode;
+                obj.ReturnMsg = "Tournament deleted successfully";
+
+            }
+            catch (Exception EX)
+            {
+
+                throw;
+            }
+            return obj;
+        }
+
+    }
+}
